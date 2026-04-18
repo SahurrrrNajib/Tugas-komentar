2b7a4de904ed23793e7e4384730255f24d3c5950
Najib Fadhilakbar <fadhilakbarnajib@gmail.com>
2026-04-18 17:02:32 +0700

    final deploy railway

--- app.py
+++ app.py
@@ -81,7 +81,8 @@ def home():
     )
 
 # =========================
-# RUN
+# RUN (WAJIB UNTUK RAILWAY)
 # =========================
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
